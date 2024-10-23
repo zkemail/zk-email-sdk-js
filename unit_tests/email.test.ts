d6766227e100d45bf42362eec9a8d702e1fad31e
Dimitri <dimitridumonet@gmail.com>
2024-10-23 17:29:13 +0700

    feat: use location header and body for testing

--- unit_tests/email.test.ts
+++ unit_tests/email.test.ts
@@ -1,5 +1,5 @@
 import { expect, test, describe, beforeAll, afterAll } from "bun:test";
-import airbnbTestEmail from "./hello_eml";
+import helloTestEmail from "./hello_eml";
 import { parseEmail, testDecomposedRegex } from "../src/utils";
 import { DecomposedRegex, DecomposedRegexPart } from "../src";
 
@@ -8,7 +8,7 @@ describe("Email utils test suite", async () => {
   await new Promise((r) => setTimeout(r, 100));
 
   test("Can parse an email", async () => {
-    const parsedEmail = await parseEmail(airbnbTestEmail);
+    const parsedEmail = await parseEmail(helloTestEmail);
     expect(parsedEmail).not.toBeNil();
   });
 
@@ -43,7 +43,7 @@ describe("Email utils test suite", async () => {
       ],
     };
 
-    const result = testDecomposedRegex(airbnbTestEmail, decomposedRegex, false);
+    const result = await testDecomposedRegex(helloTestEmail, decomposedRegex, false);
     expect(Bun.deepEquals(result, ["ZK Email"])).toBeTrue();
   });
 
@@ -68,7 +68,54 @@ describe("Email utils test suite", async () => {
       ],
     };
 
-    const result = testDecomposedRegex(airbnbTestEmail, decomposedRegex, true);
+    const result = await testDecomposedRegex(helloTestEmail, decomposedRegex, true);
     expect(Bun.deepEquals(result, ["Hello ", "ZK Email", "!"])).toBeTrue();
+  });
+
+  test("Should fail testDecomposedRegex finding body in header", async () => {
+    const decomposedRegex: DecomposedRegex = {
+      name: "Hello Pattern",
+      maxLength: 4000,
+      location: "header",
+      parts: [
+        {
+          isPublic: false,
+          regexDef: "Hello ",
+        },
+        {
+          isPublic: true,
+          regexDef: "[^,]+",
+        },
+        {
+          isPublic: false,
+          regexDef: "!",
+        },
+      ],
+    };
+
+    try {
+      await testDecomposedRegex(helloTestEmail, decomposedRegex, true);
+    } catch (err) {
+      expect(err).not.toBeUndefined();
+      return;
+    }
+    throw new Error("Found body regex in header");
+  });
+
+  test("Should find email in header in testDecomposedRegex", async () => {
+    const decomposedRegex: DecomposedRegex = {
+      name: "Find sender Pattern",
+      maxLength: 300,
+      location: "header",
+      parts: [
+        {
+          isPublic: true,
+          regexDef: "dimi.zktest@gmail.com",
+        },
+      ],
+    };
+
+    const result = await testDecomposedRegex(helloTestEmail, decomposedRegex, true);
+    expect(Bun.deepEquals(result, ["dimi.zktest@gmail.com"])).toBeTrue();
   });
 });
