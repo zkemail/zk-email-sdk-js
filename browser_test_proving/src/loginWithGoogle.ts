895cedb801c800023664f42ef1ba3ed6012edbd6
Dimitri <dimitridumonet@gmail.com>
2025-02-07 13:13:30 +0700

    feat: add basic gmail email fetching support

---
+++ browser_test_proving/src/loginWithGoogle.ts
@@ -0,0 +1,43 @@
+// import zkeSdk,  {LoginWithGoogle} from "@zk-email/sdk";
+import zkeSdk, { Gmail } from "../../src/index";
+
+export function setupLoginWithGoogle(element: HTMLElement) {
+  const sdk = zkeSdk();
+  const gmail = new Gmail();
+
+  const loginButton = element.querySelector("button");
+  if (loginButton) {
+    loginButton.addEventListener("click", async () => {
+      const blueprint = await sdk.getBlueprintById("0935faed-002d-4b94-8cbf-476b3b05d9a6");
+      // const blueprint = await sdk.getBlueprintById("008b5da5-fbda-4445-b7df-6b0c6dde4bb1");
+      // const blueprint2 = await sdk.getBlueprintById("0935faed-002d-4b94-8cbf-476b3b05d9a6");
+
+      // optional - manually start Login with Google flow and authorize before fetching emails
+      console.log("Manually authorizing");
+      await gmail.authorize();
+
+      // Will start Login with Google flow if not already autorized
+      // Fetches emails using the email queries given in the blueprints
+      console.log("fetching emails");
+      let emails = await gmail.fetchEmails([blueprint]);
+
+      // Will return an empty array if there are no more emails matching the blueprints query
+      let moreEmails = await gmail.fetchMore();
+      console.log("moreEmails: ", moreEmails);
+
+      // You can validate if an email is valid according to a blueprint
+      const email = emails[0];
+      const isValid = await blueprint.validateEmail(email.decodedContents);
+      console.log("isValid: ", isValid);
+
+      // console.log("emails: ", emails);
+      // console.log("fetching more");
+      // emails = await gmail.fetchMore();
+      // console.log("emails: ", emails);
+      try {
+      } catch (err) {
+        console.error("Failed to login with google: ", err);
+      }
+    });
+  }
+}
--- browser_test_proving/src/main.ts
+++ browser_test_proving/src/main.ts
@@ -1,5 +1,6 @@
 import "./style.css";
 import { setupProver } from "./prover.ts";
+import { setupLoginWithGoogle } from "./loginWithGoogle.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
@@ -9,7 +10,13 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
         <button class="prove">Prove</button>
       </div>
     </div>
+    <div id="lwg">
+      <div className="flex mt-5">
+        <button class="login">Login With Google</button>
+      </div>
+    </div>
   </div>
 `;
 
 setupProver(document.querySelector<HTMLElement>("#prover")!);
+setupLoginWithGoogle(document.querySelector<HTMLElement>("#lwg")!);
