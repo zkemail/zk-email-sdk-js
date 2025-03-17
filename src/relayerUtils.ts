91c3ed99d3656967afecc5b4ede29d7b7ed856fd
Dimitri <dimitridumonet@gmail.com>
2025-03-17 12:47:19 +0700

    chore: rename params

--- src/relayerUtils.ts
+++ src/relayerUtils.ts
@@ -230,7 +230,6 @@ export async function generateProofInputs(
       externalInputs,
       internalParams
     );
-    console.log("generateCircuitInputsWithDecomposedRegexesAndExternalInputs DONE: ", inputs);
 
     const json = JSON.stringify(Object.fromEntries(inputs));
     return json;
