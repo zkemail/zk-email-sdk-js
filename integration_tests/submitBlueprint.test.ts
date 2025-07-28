44f20c4b5b3bd087249b6e1f2ccc6c5d45cfe79b
Dimitri Dumonet <dimitridumonet@gmail.com>
2025-07-28 16:31:02 +0700

    chore: add simple submit test

--- integration_tests/submitBlueprint.test.ts
+++ integration_tests/submitBlueprint.test.ts
@@ -2,20 +2,20 @@ import { expect, test, describe, beforeAll, afterAll } from "bun:test";
 import zkeSdk from "../src";
 
 const sdk = zkeSdk({
-  baseUrl: "https://staging-conductor.zk.email",
+  baseUrl: "https://dev-conductor.zk.email",
   auth: {
     getToken: async () =>
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTI4MTg5MjUsImdpdGh1Yl91c2VybmFtZSI6IkRpbWlEdW1vIn0.72CrX3j0A2f3ZGZM5Ca6GnXo3_xRceryn_KsOTH8gW4",
+    "",
     onTokenExpired: async () => {},
   },
 });
 
 describe("Submit a blueprint", () => {
   test("Submit", async () => {
-    const blueprint = await sdk.getBlueprintById("ae989ffd-0586-49d5-bf8d-235bef5f7d39");
+    console.log("getting blueprint by id");
+    const blueprint = await sdk.getBlueprintById("8241f8bd-9fe7-443d-a09d-0150dcc7e85e");
+    console.log("got blueprint");
     await blueprint.submit();
     console.log("submitted");
   });
 });
-
-// const bla = [{"name":"invoice_date","parts":[{"regex_def":"INVOICE DATE<\/span><br>\s*"},{"is_public":true,"regex_def":"\d{1,2}"},{"regex_def":"\s+"},{"is_public":true,"regex_def":"\w{3}"},{"regex_def":"\s+"},{"is_public":true,"regex_def":"\d{4}"}],"location":"body","max_length":9}]
