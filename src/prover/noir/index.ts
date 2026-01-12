ca7feccf21f51f207138abb2f0993ed9c9f54025
Rute Figueiredo <rfigueiredo.dev@gmail.com>
2026-01-12 22:03:32 +0000

    feat: add support for 1024-bit RSA keys in Noir circuits
    - Auto-detect RSA key size from parsed email public key
    - Add keyBits parameter to getNoirCircuitDownloadLink,
      getNoirCircuitJsonDownloadLink, and getNoirCircuit methods
    - Use separate endpoints for 1024 and 2048 bit circuits
    - Pass rsaKeyBits to relayer-utils for input generation
    - Update @zk-email/relayer-utils to 0.4.66-17

--- src/prover/noir/index.ts
+++ src/prover/noir/index.ts
@@ -18,6 +18,17 @@ import { addMaxLengthToExternalInputs } from "../../utils/maxLenghExternalInputs
 import { logger } from "../../utils/logger";
 
 export class NoirProver extends AbstractProver implements IProver {
+  /**
+   * Detect RSA key size from parsed email public key
+   * @param parsedEmail - Parsed email object
+   * @returns 1024 or 2048
+   */
+  private detectKeySize(parsedEmail: { publicKey: Uint8Array }): 1024 | 2048 {
+    // Public key bytes: 128 bytes = 1024 bits, 256 bytes = 2048 bits
+    const keyBytes = parsedEmail.publicKey.length;
+    return keyBytes <= 128 ? 1024 : 2048;
+  }
+
   async generateLocalProof(
     eml: string,
     externalInputs: ExternalInputInput[] = [],
@@ -29,6 +40,10 @@ export class NoirProver extends AbstractProver implements IProver {
 
     const parsedEmail = await parseEmail(eml);
 
+    // Detect key size from the email
+    const keyBits = this.detectKeySize(parsedEmail);
+    logger.info(`Detected RSA key size: ${keyBits} bits`);
+
     const { Noir, UltraHonkBackend } = options.noirWasm;
 
     const startedAt = new Date();
@@ -39,7 +54,8 @@ export class NoirProver extends AbstractProver implements IProver {
       );
     }
 
-    const circuit = await this.blueprint.getNoirCircuit();
+    // Fetch the appropriate circuit based on detected key size
+    const circuit = await this.blueprint.getNoirCircuit(keyBits);
     const regexGraphs = await this.blueprint.getNoirRegexGraphs();
 
     const regexInputs = this.blueprint.props.decomposedRegexes.map((dr) => {
@@ -97,6 +113,7 @@ export class NoirProver extends AbstractProver implements IProver {
       removeSoftLineBreaks: this.blueprint.props.removeSoftLinebreaks,
       shaPrecomputeSelector: this.blueprint.props.shaPrecomputeSelector,
       proverEthAddress: "0x0000000000000000000000000000000000000000",
+      rsaKeyBits: keyBits,  // Pass key size to relayer-utils
     };
 
     logger.info("generating inputs regexInputs: ", regexInputs);
